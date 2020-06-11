e9875639ccdbbd9aa3c392be9e677b0e24cb9d5e
sudevdhali <60395148+sudevdhali@users.noreply.github.com>
2020-06-12 02:16:32 +0530

    Create script.js

---
+++ 4thweek_sol/script.js
@@ -0,0 +1,26 @@
+(function() {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (i=0;i<names.length;i++) {
+
+  var firstLetterpre =names[i].charAt(0);
+
+var firstletter=firstLetterpre.toLowerCase();
+
+  if (firstletter=="j" ) {
+
+    byeSpeaker.speak(names[i]);
+
+  } 
+
+  else {
+
+    helloSpeaker.speak(names[i]) ;
+
+  }
+
+}
+
+}) (); 
+
